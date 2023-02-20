ae80d71a8a24eb601496a222469204cf0dc2eebd
miftt <miftahfauzi012@gmail.com>
2023-02-20 17:11:54 +0700

    Tugas PHP

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,21 @@
+<?php 
+$x = "Topi";
+$y = "Bundar";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1a</title>
+</head>
+<body>
+    <h2>
+        <i>
+            <?php echo "\"$x saya $y, $y $x saya.\""; ?> 
+        </i>
+    </h2>
+</body>
+</html>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,20 @@
+<?php
+$x = 39;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1b</title>
+</head>
+<body>
+   <p><?php echo "Aku adalah angka " . "<b>" . $x . "</b>" ;?></p>
+   <p><?php echo "Jika aku dikali 5, maka aku sekarang menjadi " ."<b>" . $x*5 . "</b>" ;?></p>
+   <p><?php echo "Jika aku dibagi 2, maka aku sekarang menjadi " ."<b>" . $x/2 . "</b>" ;?></p>
+   <p><?php echo "Jika aku ditambah 75, maka aku sekarang menjadi " ."<b>" . $x+75 . "</b>" ;?></p>
+   <p><?php echo "Jika aku dikurang 20, maka aku sekarang menjadi " ."<b>" . $x-20 . "</b>" ;?></p>
+</body>
+</html>
